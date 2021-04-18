ac910517ba5985680f5ea81cd4687e6f2a093ae5
Michanix <michanix@protonmail.com>
2021-04-18 22:44:25 +0300

    Added form for bust stops

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import axios from 'axios';
 
 import StopAreaForm from './StopAreaForm';
+import BusStopForm from './BusStopForm';
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.baseUrl = "http://localhost:8080/api/v1/stops";
+        this.baseUrl = "http://localhost:8080/api/v1/bus-stops";
         this.state = {
             areas: []
         }
@@ -14,7 +15,7 @@ export default class Home extends React.Component {
 
     componentDidMount() {
         axios
-            .get(this.baseUrl + "/areas/")
+            .get(this.baseUrl + "/areas")
             .then(res => {
                 this.setState({
                     areas: res.data
@@ -31,6 +32,7 @@ export default class Home extends React.Component {
         const areas = this.state.areas;
         return (
             <StopAreaForm areas={areas} />
+            // <BusStopForm areas={areas} />
         );
     }
 }
--- client/src/components/StopAreaForm.js
+++ client/src/components/StopAreaForm.js
@@ -4,44 +4,91 @@ import axios from 'axios';
 export default class StopAreaForm extends React.Component {
     constructor(props) {
         super(props);
-        this.baseUrl = "http://localhost:8080/api/v1/stops";
+        this.baseUrl = "http://localhost:8080/api/v1/bus-stops";
         this.state = {
             area: "",
+            stop: "",
+            stops: [],
+            busNumbers: [],
         }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleAreaChange = this.handleAreaChange.bind(this);
+        this.handleAreaSubmit = this.handleAreaSubmit.bind(this);
+        this.handleStopChange = this.handleStopChange.bind(this);
+        this.handleStopSubmit = this.handleStopSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({area: event.target.value});
+    handleAreaChange(event) {
+        this.setState({ area: event.target.value });
     }
 
-    handleSubmit(event) {
+    handleAreaSubmit(event) {
         const area = this.state.area;
+        const url = this.baseUrl + "/areas/" + area + "/stops"
         axios
-        .post(this.baseUrl + "/areas/", {
-            area: area,
-        })
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+            .get(url)
+            .then(res => this.setState({ stops: res.data }))
+            .catch(err => console.log(err));
+        event.preventDefault();
     }
+
+    handleStopChange(event) {
+        this.setState({ stop: event.target.value })
+    }
+
+    handleStopSubmit(event) {
+        const busStop = this.state.stop;
+        const url = this.baseUrl + "/" + busStop + "/bus-numbers";
+        axios
+            .get(url)
+            .then(res => this.setState({ busNumbers: res.data }))
+            .catch(err => console.log(err))
+        event.preventDefault();
+    }
+
 
     render() {
         const areasList = this.props.areas.map((area) => {
             return <option>{area}</option>
         })
+        const stopsList = this.state.stops.map(stop => {
+            return <option>{stop}</option>
+        })
+        let busNumsList;
+
+        if (this.state.busNumbers.length > 0) {
+            busNumsList = this.state.busNumbers.map(num => {
+                return <button>{num}</button>
+            })
+        } else {
+            busNumsList = <p>No available buses.</p>
+        }
 
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Pick you area: 
-                    <select value={this.state.area} onChange={this.handleChange}>
-                        {areasList}
-                    </select>
-                </label>
-                <input type='submit' value='Submit' />
-            </form>
+            <div>
+                <form onSubmit={this.handleAreaSubmit}>
+                    <label>
+                        Pick your area:
+                    <select value={this.state.area} onChange={this.handleAreaChange}>
+                            {areasList}
+                        </select>
+                    </label>
+                    <input type='submit' value='Submit' />
+                </form>
+
+
+                <form onSubmit={this.handleStopSubmit}>
+                    <label>
+                        Pick your bus stop:
+                    <select value={this.state.stop} onChange={this.handleStopChange}>
+                            {stopsList}
+                        </select>
+                    </label>
+                    <input type='submit' value='Submit' />
+                </form>
+                {busNumsList}
+            </div>
+
         )
     }
 }
